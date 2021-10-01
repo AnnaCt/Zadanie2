914ac4cc753353824cf0eff3d4067d6329246f74
Annachernova003@mail.ru <you@example.com>
2021-10-01 13:21:40 +0300

    First commit

---
+++ 1.py
@@ -0,0 +1,5 @@
+name = input()
+surname = input()
+date_of_birth = input()
+print(name, '_', surname, '_', int(date_of_birth)+60)
+
---
+++ 2.py
@@ -0,0 +1,9 @@
+print ("Введите a")
+a = float(input())
+print ("Введите b")
+b = float(input())
+c = (a ** 2 + b ** 2) ** 0.5
+P = a + b + c
+S = (a ** b)/2
+print ("Периметр=" + str(int(P)))
+print ("Площадь=" + str(int(S)))
